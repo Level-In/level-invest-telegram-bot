8651aa19fd2c21ef943da62f6a2206750d9de42c
Level-In <levelinvest.bok@gmail.com>
2026-06-04 19:39:27 +0200

    Update send_message.py

--- send_message.py
+++ send_message.py
@@ -1,8 +1,6 @@
 import os
 import requests
 
-print("TEST NOWEJ WERSJI")
-
 TOKEN = os.environ["TELEGRAM_TOKEN"]
 
 CHAT_IDS = [
@@ -20,21 +18,10 @@ Przypominamy o aktualizacji aktywności do godziny 20:00 ⏰📲
 Trzymamy tempo i działamy dalej! 🚀"""
 
 for chat_id in CHAT_IDS:
-    try:
-        response = requests.post(
-            f"https://api.telegram.org/bot{TOKEN}/sendMessage",
-            data={
-                "chat_id": chat_id,
-                "text": MESSAGE
-            },
-            timeout=30
-        )
-
-        print(
-            f"CHAT: {chat_id} | STATUS: {response.status_code} | ODPOWIEDŹ: {response.text}"
-        )
-
-    except Exception as e:
-        print(f"BŁĄD DLA {chat_id}: {e}")
-
-print("KONIEC PROGRAMU")
+    requests.post(
+        f"https://api.telegram.org/bot{TOKEN}/sendMessage",
+        data={
+            "chat_id": chat_id,
+            "text": MESSAGE
+        }
+    )
