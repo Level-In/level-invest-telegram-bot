999ef4671c07209c726faf8cf48d4efd881d0e7a
Level-In <levelinvest.bok@gmail.com>
2026-06-04 19:32:18 +0200

    Update send_message.py

--- send_message.py
+++ send_message.py
@@ -1,12 +1,14 @@
 import os
 import requests
 
+print("TEST NOWEJ WERSJI")
+
 TOKEN = os.environ["TELEGRAM_TOKEN"]
 
 CHAT_IDS = [
-    "-1004231126426",  # GDAŃSK
-    "-1004249984029",  # WARSZAWA
-    "-1003932802265"   # KRAKÓW
+    "-1004231126426",
+    "-1004249984029",
+    "-1003932802265"
 ]
 
 MESSAGE = """Dziękujemy za dzisiejszy dzień! 💛
@@ -18,13 +20,21 @@ Przypominamy o aktualizacji aktywności do godziny 20:00 ⏰📲
 Trzymamy tempo i działamy dalej! 🚀"""
 
 for chat_id in CHAT_IDS:
-    response = requests.post(
-        f"https://api.telegram.org/bot{TOKEN}/sendMessage",
-        data={
-            "chat_id": chat_id,
-            "text": MESSAGE
-        }
-    )
-    print(chat_id, response.status_code, response.text)
+    try:
+        response = requests.post(
+            f"https://api.telegram.org/bot{TOKEN}/sendMessage",
+            data={
+                "chat_id": chat_id,
+                "text": MESSAGE
+            },
+            timeout=30
+        )
 
-print("Wiadomości wysłane")
+        print(
+            f"CHAT: {chat_id} | STATUS: {response.status_code} | ODPOWIEDŹ: {response.text}"
+        )
+
+    except Exception as e:
+        print(f"BŁĄD DLA {chat_id}: {e}")
+
+print("KONIEC PROGRAMU")
